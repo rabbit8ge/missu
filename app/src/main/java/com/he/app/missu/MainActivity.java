773ca8dcf2d3ea23a7fb75b01df10624821ac729
hyp <yipinghe87@163.com>
2015-09-27 12:03:12 +0800

    Compile success.

--- app/src/main/java/com/he/app/missu/MainActivity.java
+++ app/src/main/java/com/he/app/missu/MainActivity.java
@@ -1,18 +1,20 @@
 package com.he.app.missu;
 
-import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.he.app.tools.BaseActivity;
 
-public class MainActivity extends Activity {
+
+public class MainActivity extends BaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
